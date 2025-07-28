305333cf5be0164eaf7e12d13f5b798684883e97
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-07-28 14:52:13 -0300

    fix: add annotations for entities

--- src/main/java/com/jeffersonsousa/smartstock/entity/Product.java
+++ src/main/java/com/jeffersonsousa/smartstock/entity/Product.java
@@ -1,6 +1,9 @@
 package com.jeffersonsousa.smartstock.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,6 +18,8 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Product {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String id;
 	private String name;
 	private String currentQuantity;
--- src/main/java/com/jeffersonsousa/smartstock/entity/StockMovement.java
+++ src/main/java/com/jeffersonsousa/smartstock/entity/StockMovement.java
@@ -3,6 +3,9 @@ package com.jeffersonsousa.smartstock.entity;
 import java.time.Instant;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,6 +20,8 @@ import lombok.Setter;
 @AllArgsConstructor
 public class StockMovement {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String type;
 	private Double quantity;
