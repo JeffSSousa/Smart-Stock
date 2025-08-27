cfafc9d734a334e800d10a2cb38ca69196888159
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-08-26 23:06:58 -0300

    test: add test units for update and delete in ProductService

--- src/test/java/com/jeffersonsousa/smartstock/service/ProductServiceTest.java
+++ src/test/java/com/jeffersonsousa/smartstock/service/ProductServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -21,12 +23,15 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import com.jeffersonsousa.smartstock.dto.ProductRequestDTO;
 import com.jeffersonsousa.smartstock.dto.ProductResponseDTO;
+import com.jeffersonsousa.smartstock.dto.ProductUpdateDTO;
 import com.jeffersonsousa.smartstock.entity.Category;
 import com.jeffersonsousa.smartstock.entity.Product;
 import com.jeffersonsousa.smartstock.exception.ControllerNotFoundException;
+import com.jeffersonsousa.smartstock.exception.DatabaseException;
 import com.jeffersonsousa.smartstock.repository.CategoryRepository;
 import com.jeffersonsousa.smartstock.repository.ProductRepository;
 
@@ -184,18 +189,94 @@ public class ProductServiceTest {
 	@Nested
 	class update{
 		//update
-		//não encontrado
-	}
-	
-	@Nested
-	class updateDate{
-		//suceess
+		
+		@Test
+		@DisplayName("Deve Atualizar dados do produto com sucesso.")
+		void shouldUpdateAProduct() {
+			Long id = 1L;
+			Category category = new Category(id, "Computers", null);
+			Product iphone = new Product(1L, "Apple iPhone 15", 10, 10, 10996.0, category, null, null);
+			ProductUpdateDTO updateDTO = new ProductUpdateDTO("Apple iPhone 15 Pro Max", 15, 12289.0);
+			when(productRepository.findById(id)).thenReturn(Optional.of(iphone));
+			
+			
+			service.update(id, updateDTO);
+			
+			
+			verify(productRepository, times(1)).findById(id);
+			verify(productRepository, times(1)).save(captor.capture());
+			
+			Product output = captor.getValue();
+			
+			assertEquals(updateDTO.name(), output.getName());
+			assertEquals(updateDTO.minimumQuantity(), output.getMinimumQuantity());
+			assertEquals(updateDTO.price(), output.getPrice());
+		}
+		
+		
+		@Test
+		@DisplayName("Deve lançar uma exceção se não encontrar o produto.")
+		void shouldThrowExceptionWhenProductNotFound() {
+			
+			Long id = 1L;
+			ProductUpdateDTO updateDTO = new ProductUpdateDTO("Apple iPhone 15 Pro Max", 15, 12289.0);
+			when(productRepository.findById(id)).thenReturn(Optional.empty());
+			
+			ControllerNotFoundException e = assertThrows(ControllerNotFoundException.class, () -> service.update(id, updateDTO));
+			
+			verify(productRepository, times(1)).findById(id);
+			assertEquals("Produto com o ID" + id + " não foi encontrado!!", e.getMessage());
+		}
 	}
 	
 	@Nested
 	class delete{
-		//delete
-		//not found
-		//integrityViolation
+		
+		@Test
+		@DisplayName("Deve deletar uma categoria com sucesso.")
+		void shouldDeleteAProduct() {
+			
+			Long id = 1L;
+			Category category = new Category(id, "Computers", null);
+			Product iphone = new Product(1L, "Apple iPhone 15", 10, 10, 10996.0, category, null, null);
+			
+			when(productRepository.findById(id)).thenReturn(Optional.of(iphone));
+			doNothing().when(productRepository).delete(iphone);
+			
+			service.delete(id);
+			
+			verify(productRepository, times(1)).findById(id);
+			verify(productRepository, times(1)).delete(iphone);
+		}
+		
+		@Test
+		@DisplayName("Deve lançar uma exceção se não encontrar o produto.")
+		void shouldThrowExceptionWhenProductNotFound() {
+			
+			Long id = 1L;
+			when(productRepository.findById(id)).thenReturn(Optional.empty());
+			
+			ControllerNotFoundException e = assertThrows(ControllerNotFoundException.class, () -> service.delete(id));
+			
+			verify(productRepository, times(1)).findById(id);
+			assertEquals("Produto com o ID" + id + " não foi encontrado!!", e.getMessage());
+		}
+		
+		@Test
+		@DisplayName("Deve lançar uma exceção quando o produto estiver vinculado com outras entidades.")
+		void shouldThrowExceptionWhenProductIsLinkedToOtherEntities() {
+			
+			Long id = 1L;
+			Category category = new Category(id, "Computers", null);
+			Product iphone = new Product(1L, "Apple iPhone 15", 10, 10, 10996.0, category, null, null);
+			
+			when(productRepository.findById(id)).thenReturn(Optional.of(iphone));
+			doThrow(DataIntegrityViolationException.class).when(productRepository).delete(iphone);
+			
+			DatabaseException e = assertThrows(DatabaseException.class, () -> service.delete(id));
+			
+			assertEquals("Não é possível excluir o produto. Ele está vinculado a outras entidades no sistema.", e.getMessage());		
+		}
+	
 	}
 }
