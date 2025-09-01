af4c044262d2c801cf1db2f020412e9028036b3f
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-09-01 15:44:25 -0300

    test: add unit tests for output in StockMovementService

--- src/test/java/com/jeffersonsousa/smartstock/service/StockMovementServiceTest.java
+++ src/test/java/com/jeffersonsousa/smartstock/service/StockMovementServiceTest.java
@@ -1,13 +1,17 @@
 package com.jeffersonsousa.smartstock.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
@@ -26,6 +30,7 @@ import com.jeffersonsousa.smartstock.entity.Location;
 import com.jeffersonsousa.smartstock.entity.Product;
 import com.jeffersonsousa.smartstock.entity.StockMovement;
 import com.jeffersonsousa.smartstock.exception.ControllerNotFoundException;
+import com.jeffersonsousa.smartstock.exception.InsufficientStockException;
 import com.jeffersonsousa.smartstock.exception.StockException;
 import com.jeffersonsousa.smartstock.repository.LocationRepository;
 import com.jeffersonsousa.smartstock.repository.ProductRepository;
@@ -51,6 +56,12 @@ public class StockMovementServiceTest {
 	
 	@Captor
 	private ArgumentCaptor<StockMovement> captorStockMovement;
+	
+	@Captor
+	private ArgumentCaptor<List<Location>> captorStockLocation;
+	
+	@Captor
+	private ArgumentCaptor<List<StockMovement>> captorNewStockMovements;
 	
 	@Nested
 	class input{
@@ -104,8 +115,9 @@ public class StockMovementServiceTest {
 		@DisplayName("Deve lançar uma exceção quando o produto não for encontrado.")
 		void shouldThrowExceptionWhenProductNotFound() {
 			
-			MovementRequestDTO dto = new MovementRequestDTO(10, 1L);
-			when(productRepository.findById(dto.productId())).thenReturn(Optional.empty());
+			Long id = 1L;
+			MovementRequestDTO dto = new MovementRequestDTO(10, id);
+			when(productRepository.findById(id)).thenReturn(Optional.empty());
 			
 			ControllerNotFoundException e = assertThrows(ControllerNotFoundException.class, () -> service.Input(dto));
 			
@@ -135,10 +147,92 @@ public class StockMovementServiceTest {
 	
 	@Nested
 	class output{
-		//output success
-		//não foi encontrado produto
-		//saldo insufiente no estoque
-		// quantiti deve ser maior que 0
+		
+		@Test
+		@DisplayName("Deve realizar a saída de um produto do estoque atualizando localizações e registrando movimentações.")
+		void shouldRemoveProductsFromStock() {
+			
+			MovementRequestDTO dto = new MovementRequestDTO(10, 1L);
+			Category category = new Category(1L, "Computer", null);
+			Product product = new Product(1L, "iPhone 15", 15, 10, 4999.9, category, null, null);
+			Location location = new Location("A.1.1", 'A', 1, 1, true, product, 9, null);
+			Location location2 = new Location("A.1.2", 'A', 1, 2, true, product, 6, null);
+			List<Location> stock = List.of(location,location2);
+			
+			when(productRepository.findById(dto.productId())).thenReturn(Optional.of(product));
+			when(locationRepository.findByProduct(product)).thenReturn(stock);
+			
+			service.Output(dto);
+			
+			verify(productRepository, times(1)).findById(any());
+			verify(locationRepository, atLeastOnce()).findByProduct(any(Product.class));
+			verify(locationRepository, times(1)).saveAll(captorStockLocation.capture());
+			verify(movementRepository, times(1)).saveAll(captorNewStockMovements.capture());
+		
+			List<Location> locationsOutput = captorStockLocation.getValue();
+			assertEquals(0, locationsOutput.get(0).getQuantity());
+			assertFalse(locationsOutput.get(0).getHasProduct());
+			assertEquals(5, locationsOutput.get(1).getQuantity());
+			assertTrue(locationsOutput.get(1).getHasProduct());
+			
+			List<StockMovement> stockMovementsOutput = captorNewStockMovements.getValue();
+			assertEquals(2, stockMovementsOutput.size());
+			assertEquals(9, stockMovementsOutput.get(0).getQuantity());
+			assertEquals(1, stockMovementsOutput.get(1).getQuantity());	
+			assertEquals("Saída", stockMovementsOutput.get(0).getType());
+			assertNotNull(stockMovementsOutput.get(0).getMovimentationDate());
+			assertEquals(product.getName(), stockMovementsOutput.get(0).getProduct().getName());
+			assertEquals(location.getLocationId(), stockMovementsOutput.get(0).getLocation().getLocationId());
+			
+			assertEquals(5, product.getCurrentQuantity());
+		}
+		
+		@Test
+		@DisplayName("Deve lançar exceção quando o produto não for encontrado.")
+		void shouldThrowExceptionWhenProductNotFound() {
+			
+			Long id = 1L;
+			MovementRequestDTO dto = new MovementRequestDTO(10, id);
+			
+			when(productRepository.findById(id)).thenReturn(Optional.empty());
+			
+			ControllerNotFoundException e = assertThrows(ControllerNotFoundException.class, () -> service.Output(dto));
+			
+			assertEquals("Produto com o ID " + dto.productId() + " não foi encontrado!!", e.getMessage());	
+		}
+		
+		
+		@Test
+		@DisplayName("Deve lançar exceção quando o saldo for insulficiente no estoque.")
+		void shoulThrowExceptionWhenTheBalanceIsInsufficientInStock() {
+			
+			Long id = 1L;
+			MovementRequestDTO dto = new MovementRequestDTO(10, 1L);
+			Category category = new Category(1L, "Computer", null);
+			Product product  = new Product(1L, "iPhone 15 Pro Max", 9, 10, 12266.2, category, null, null);
+			
+			when(productRepository.findById(id)).thenReturn(Optional.of(product));
+			
+			InsufficientStockException e = assertThrows(InsufficientStockException.class, () -> service.Output(dto));
+			
+			assertEquals("Saldo Insuficiente no Estoque!!", e.getMessage());
+		}
+		
+		@Test
+		@DisplayName("Deve lançar exceção quando a quantidade for menor ou igual a zero.")
+		void shouldThrowExceptionWhenQuantityIsLessThanOrEqualToZero() {
+			
+			Long id = 1L;
+			MovementRequestDTO dto = new MovementRequestDTO(0, 1L);
+			Category category = new Category(1L, "Computer", null);
+			Product product  = new Product(1L, "iPhone 15 Pro Max", 9, 10, 12266.2, category, null, null);
+			
+			when(productRepository.findById(id)).thenReturn(Optional.of(product));
+		
+			StockException e = assertThrows(StockException.class, () -> service.Output(dto));
+			
+			assertEquals("A quantidade de saída deve ser maior que zero!!", e.getMessage());
+		}
 	}
 	
 }
